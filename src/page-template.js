c065dfbb348afa470997a868252a5fbe855dd6e6
Vaggelis <evangelodamianos@gmail.com>
2022-03-04 16:27:52 -0500

    bootstrap added

--- src/page-template.js
+++ src/page-template.js
@@ -4,17 +4,16 @@ const generatePage = () => {
   return `
   <!DOCTYPE html>
   <html lang="en">
-
   <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
+    <link rel="stylesheet" href="style.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Team Profile Generator</title>
   </head>
-
   <body>
-    <h1>${generatePage.name}</h1>
-    <h2><a href="https://github.com/${generatePage.github}">Github</a></h2>
+    <header class ="bg-danger height"><h1 class="text-light text-center">My Team</h1></header>
   </body>
   </html>
   `;
