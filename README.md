e77786ba9810e6a745f9a7e80cd29f13a9c83e0c
Vaggelis <evangelodamianos@gmail.com>
2022-03-10 04:20:18 -0500

    Final touches

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 The team profile generator is an application which uses inquirer to input user data which is then used to create an html page of the team you've created.
 
 ## Walkthrough Video
+
+https://drive.google.com/file/d/1kmwwr-HNFAV9Jue1EWnL3CGZIZXabcel/view
--- src/page-template.js
+++ src/page-template.js
@@ -4,41 +4,41 @@ const Manager = require('../lib/Manager');
 
 const generatePage = (teamGen) => {
   return `
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Team Profile Generator</title>
-  </head>
-  <body>
-    <header class ="bg-danger height">
-      <h1 class="text-light text-location">My Team</h1>
-    </header>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<title>Team Profile Generator</title>
+</head>
+<body>
+  <header class ="bg-danger height">
+    <h1 class="text-light text-location">My Team</h1>
+  </header>
 
-    <section class="d-flex justify-content-between m-5">
+  <section class="content-holder">
     ${teamGen.join('')}
-    </section>    
-  </body>
-  </html>
+  </section>    
+</body>
+</html>
   `;
 };
 
 const generateManager = (obj) =>
 {
   return `
-  <section class="shadow card" style="width: 18rem;">
+  <section class="shadow card card-prop" style="width: 18rem;">
   <div class = "card-body">
     <div class="card-title">
       <h2 class="text-light">${obj.getName()}</h2>
       <h2 class="text-light">${obj.getRole()}</h2>
     </div>
-    <ul class="list-inline mx-auto justify-content-center m-3">
+    <ul class="list-inline mx-auto justify-content-center">
       <li class="border p-2 bg-white rounded">ID: ${obj.getId()}</li>
-      <li class="border p-2 bg-white rounded">Email: <a href="mailto${obj.getEmail()}:">${obj.getEmail()}</a></li>
+      <li class="border p-2 bg-white rounded">Email: <a href="mailto:${obj.getEmail()}:">${obj.getEmail()}</a></li>
       <li class="border p-2 bg-white rounded">Office number: ${obj.officeNumber}</li>
     </ul>
   </div>
@@ -49,15 +49,15 @@ const generateManager = (obj) =>
 const generateEngineer = (obj) =>
 {
   return `
-  <section class="shadow card" style="width: 18rem;">
+  <section class="shadow card card-prop" style="width: 18rem;">
   <div class = "card-body">
     <div class="card-title">
       <h2 class="text-light">${obj.getName()}</h2>
       <h2 class="text-light">${obj.getRole()}</h2>
     </div>
-    <ul class="list-inline mx-auto justify-content-center m-3">
+    <ul class="list-inline mx-auto justify-content-center">
       <li class="border p-2 bg-white rounded">ID: ${obj.getId()}</li>
-      <li class="border p-2 bg-white rounded">Email: <a href="mailto${obj.getEmail()}:">${obj.getEmail()}</a></li>
+      <li class="border p-2 bg-white rounded">Email: <a href="mailto:${obj.getEmail()}:">${obj.getEmail()}</a></li>
       <li class="border p-2 bg-white rounded">Github: <a href="http://github.com/${obj.getGithub()}" target="_blank" rel="noopener noreferrer">${obj.getGithub()}</a></li>
     </ul>
   </div>
@@ -69,15 +69,15 @@ const generateEngineer = (obj) =>
 const generateIntern = (obj) =>
 {
   return `
-  <section class="shadow card" style="width: 18rem;">
+  <section class="shadow card card-prop" style="width: 18rem;">
   <div class = "card-body">
     <div class="card-title">
       <h2 class="text-light">${obj.getName()}</h2>
       <h2 class="text-light">${obj.getRole()}</h2>
     </div>
-    <ul class="list-inline mx-auto justify-content-center m-3">
+    <ul class="list-inline mx-auto justify-content-center">
       <li class="border p-2 bg-white rounded">ID: ${obj.getId()}</li>
-      <li class="border p-2 bg-white rounded">Email: <a href="mailto${obj.getEmail()}:">${obj.getEmail()}</a></li>
+      <li class="border p-2 bg-white rounded">Email: <a href="mailto:${obj.getEmail()}:">${obj.getEmail()}</a></li>
       <li class="border p-2 bg-white rounded">School: ${obj.getSchool()}</li>
     </ul>
   </div>
